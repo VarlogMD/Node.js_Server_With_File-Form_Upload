6e9c19ea9dda4d9506e39727872dab9b8ef6d848
VarlogMD <demike1987@hotmail.com>
2021-09-22 12:57:28 +0300

    file upload works

--- app.js
+++ app.js
@@ -3,20 +3,26 @@ const fs = require('fs');
 const path = require('path');
 const { sensitiveHeaders } = require('http2');
 
+
+//create server
 const httpServer = http.createServer( (req, res) => {
     console.log(`req: ${req.url}`);
     if (req.url === '/') {
         sendRes('index.html', 'text/html', res);  
+    } else if (/\/uploads\/[^\/]+$/.test(req.url) && req.method === 'POST') {
+        console.log('upload files');
+        saveUploadFile(req, res);
     } else {
         sendRes(req.url, getContentType(req.url), res)
     }  
 
 }).listen(3000, ()=> {
-    console.log('node.js port 3000');
+    console.log('rever started at port 3000');
     
 });
 
 
+//sendRes (sending resources)
 function sendRes(url, contentType, res) {
     let file = path.join(__dirname+ '/static/', url);
     fs.readFile(file, (err, content) => {
@@ -34,6 +40,8 @@ function sendRes(url, contentType, res) {
     })
 }
 
+
+//getting type of content 
 function getContentType (url) {
     switch (path.extname(url)) {
         case ".html" :
@@ -48,4 +56,28 @@ function getContentType (url) {
             return "aplication/octet-stream"                  
     }
 
+}
+
+//saving file
+function saveUploadFile(req, res) {
+    let fileName = path.basename(req.url);
+    let file = path.join(__dirname, 'uploads', fileName);
+    let imageFolder = path.join(__dirname, 'static/images', fileName);
+
+    req.pipe(fs.createWriteStream(file));
+    req.on('end', () => {
+        fs.copyFile(file, imageFolder, err => {
+            if(err) {
+                console.log(err);
+            }
+            fs.unlink(file, err => {
+                if(err) {
+                    console.log(err);
+                }
+            })
+        })
+        res.writeHead(200, {'Content-Type': 'text'});
+        res.write(fileName);
+        res.end();
+    })
 }
--- static/js/upload.js
+++ static/js/upload.js
@@ -1,15 +1,15 @@
 function uploadFile(event) {
-    console.log('inside upload file')
+    console.log('load file start');
     let target = event.target || event.srcElement || event.currentTarget;
     let file = target.files[0];
     let xhr = new XMLHttpRequest();
     xhr.open('POST', '/uploads/' + file.name, true);
-    xhr.setRequestHeader('Content-Type', 'aplication/octet-stream');
+    xhr.setRequestHeader('Content-Type', 'application/octet-stream');
     xhr.onreadystatechange = function () {
         event = null;
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-                console.log('sucsess');
+                callbackFunction(this.responseText);
             } else {
                 console.log('error');
             }
@@ -17,6 +17,12 @@ function uploadFile(event) {
     }
     xhr.send(file);
     event.target.value = "";
+}
+
+function callbackFunction(data) {
+    console.log(data);
+    document.querySelector('.icon-image').src = 'images/'+data;
+    document.querySelector('input[name="imagename"]').value = data;
 }
 
 
